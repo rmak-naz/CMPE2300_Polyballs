a41bfcb871024972f76845321d691d0f07101f6b
rmak-naz <40699936+rmak-naz@users.noreply.github.com>
2018-11-29 19:07:48 -0700

    Add files via upload

---
+++ ICA15_Polyballs/CBallDerived.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GDIDrawer;
+using BallBase;
+using System.Drawing;
+
+namespace ICA15_Polyballs
+{
+    class CBallDerived : CBall
+    {
+        private int _time;
+        public CBallDerived(PointF initPos, double dheading, double dSpeed, int xSize, int ySize, int time)
+            :base(initPos, dheading, dSpeed, xSize, ySize)
+        {
+            _time = time;
+        }
+
+        public override void Move()
+        {
+            _time -= 1;
+            
+            base.Move();
+        }
+
+        public override void Render(CDrawer dr)
+        {
+            if (_time > 0)
+            {
+                
+                base.Render(dr);
+                dr.AddText(_time.ToString(), 10, (int)(Pos.X - 30), (int)(Pos.Y - 10), 60, 30, Color.Yellow);
+            }
+            
+        }
+    }
+}
---
+++ ICA15_Polyballs/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GDIDrawer;
+using BallBase;
+
+namespace ICA15_Polyballs
+{
+    public partial class Form1 : Form
+    {
+        CDrawer canvas;
+        List<CBall> ballList = new List<CBall>();
+        Random rnd = new Random();
+        private delegate void delVoidCDrawerPoint(Point pos, CDrawer dr);
+
+        public Form1()
+        {
+            InitializeComponent();
+            canvas = new CDrawer();
+            canvas.MouseLeftClick += Canvas_MouseLeftClick;
+            canvas.MouseRightClick += Canvas_MouseRightClick;
+        }
+
+        private void Canvas_MouseRightClick(Point pos, CDrawer dr)
+        {
+            Invoke(new delVoidCDrawerPoint(GetRightClick), pos, dr);
+        }
+
+        private void Canvas_MouseLeftClick(Point pos, CDrawer dr)
+        {
+            Invoke(new delVoidCDrawerPoint(GetLeftClick), pos, dr);
+        }
+
+        private void GetLeftClick(Point pos, CDrawer dr)
+        {
+            CBall newBall;
+
+            newBall = new CBall(pos, rnd.Next(1, 366));
+            ballList.Add(newBall);
+            newBall.Render(dr);
+        }
+
+        private void GetRightClick(Point pos, CDrawer dr)
+        {
+            CBallDerived newDBall;
+
+            newDBall = new CBallDerived(pos, rnd.Next(1, 366), 2.5, 800, 600, 20);
+            ballList.Add(newDBall);
+            newDBall.Render(dr);
+        }
+
+        private void RenderTimer_Tick(object sender, EventArgs e)
+        {
+            canvas.Clear();
+            ballList.ForEach(ball => ball.Move());
+            ballList.ForEach(ball => ball.Render(canvas));
+            //foreach (CBall ball in ballList)
+            //{
+            //    if (ball is CBallDerived)
+            //    {
+            //        ((CBallDerived)ball).Move();
+            //        ((CBallDerived)ball).Render(canvas);
+            //    }
+            //    else
+            //    {
+            //        ball.Move();
+            //        ball.Render(canvas);
+            //    }
+            //}
+        }
+    }
+}
